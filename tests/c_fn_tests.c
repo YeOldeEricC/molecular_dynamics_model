a6466fbe6c33d1d8773f651aefd799b3095ab888
YeOldeEricC <eachapman26@gmail.com>
2021-10-30 16:29:02 +0100

    ...y

--- tests/c_fn_tests.c
+++ tests/c_fn_tests.c
@@ -9,7 +9,7 @@
 #include <tgmath.h>
 #include <string.h>
 #include <complex.h>
-
+// dot product of two vectors
 double dot(double *v0, double *v1) {
 	double outp = 0;
 	for (int i = 0; i < 3; i++) {
@@ -17,15 +17,32 @@ double dot(double *v0, double *v1) {
 	}
 	return outp;
 }
-
-void add_vec(double *v0, double *v1, double *result) {
+// add two vectors
+void add(double *v0, double *v1, double *result) {
 	for (int i = 0; i < 3; i++) {
 		result[i] = (v0[i] + v1[i]);
 	}
 }
-
+// discriminant for my phi version of quadratics
+double D_phi(double a, double b, double c) {
+	return pow(b,2) - (a*c);
+}
+// calc xyz comps of a line
 double line_comp(double t, double m, double d) {
 	return (m*t) - d;
+}
+
+void t_intersects(double *m, double *d, double *o, double r, double complex *t_out) {
+	// phi_a
+	double pa = dot(m,m);
+	// phi_b
+	double dpo[3];
+	add(d,o,dpo);
+	double pb = dot(m,dpo);
+	// phi_c
+	double pc = dot(d,d) + dot(o,o) + (2*dot(d,o)) - pow(r,2);
+	// discriminant
+
 }
 
 int main() {
@@ -34,13 +51,24 @@ int main() {
 	// use that assumption for the creation of functions.
 
 	// grad of line components
+	double mx = 0.05;
+	double my = 0;
+	double mz = -0.05;
+	double m[3] = {mx,my,mz};
+	// line component offsets
+	double dx = -0.1;
+	double dy = 0.05;
+	double dz = -0.05;
+	double d[3] = {dx,dy,dz};
+	// sphere params, orig & radius
+	double ox = 0.5;
+	double oy = 0.25;
+	double oz = -0.25;
+	double o[3] = {ox,oy,oz};
+	double r = 1; // radius
 
+	double complex t_ints[2];
 
-	double dot_prod = dot(vec0,vec1);
-	add_vec(vec0,vec1,res);
-
-	printf("dot prod: %f\n",dot_prod);
-	printf("vec add: [%f,%f,%f]\n",res[0],res[1],res[2]);
 
 
 	printf("\n");
